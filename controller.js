2f76e71a8710a7efe8c0b3c2e5ede8f0309b491e
dallin-r-parker <dallin.r.parker@gmail.com>
2016-10-20 16:06:37 -0600

    new files

--- controller.js
+++ controller.js
@@ -1,10 +1,13 @@
-angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-  $scope.getUsers = function() {
-    mainService.getUsers().then(function(dataFromService) {
-      $scope.users = dataFromService.data.data;
-    });
-  }
+var app = angular.module('userProfiles');
 
-  $scope.getUsers();
-
+//.controller establishes what's within the function as a controller
+//'MainController' is the name of the controller we use to reference it throughout the program
+//inside the func's parameters $scope makes it so 
+app.controller('MainController', function($scope, mainService) {
+    $scope.getUsers = function () {
+        mainService.getUsers().then(function (dataFromService) {
+            $scope.users = dataFromService;
+        });
+    }
+    $scope.getUsers();
 });
